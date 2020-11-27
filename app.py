c3b58f3d806cfa6d97369f8cf2a9358c03f57275
Efraín Córdova <52300931+ecordova0975@users.noreply.github.com>
2020-11-27 11:44:53 -0500

    Fixed minor issues

--- app.py
+++ app.py
@@ -4,11 +4,23 @@ app = Flask(__name__)
 
 @app.route("/"):
 def index():
-	return render_template("home.html")
+	return render_template("index.html")
+
+@app.route("/home"):
+def home():
+	#Handle main activity (display totemiq art)	
+
+@app.route("/info"):
+def info():
+	#Handle display info about the proyect	
 
 @app.route("/login"):
 def login():
 	#Handle login (if we are gonna do that)	
+
+@app.route("/logout"):
+def login():
+	#Handle logout (if we are gonna do that)		
 
 if __name__ == "__main__":
 	app.run(debug=True)
