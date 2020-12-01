e0c725557fce5f29676c53719c46f448623e7e2d
Efraín Córdova <52300931+ecordova0975@users.noreply.github.com>
2020-11-30 21:51:32 -0500

    Mandatory push of branch movement ._.

--- db_conn.py
+++ db_conn.py
@@ -8,17 +8,17 @@ DB_USER = 'postgres'
 DB_PASS = 'niarfe+456'
 DB_PORT =  5433
 
-try:
-	conn = psycopg2.connect(dbname=DB_NAME, user=DB_USER, password=DB_PASS, host=DB_HOST , port=DB_PORT)
-	cur = conn.cursor()
-	#conn properties
-	print (conn.get_dsn_parameters(),"\n")
+def push_artist():
+	try:
+		conn = psycopg2.connect(dbname=DB_NAME, user=DB_USER, password=DB_PASS, host=DB_HOST , port=DB_PORT)
+		cur = conn.cursor()
+		cur.execute("INSERT INTO ")
 
-except (Exception, psycopg2.Error) as error :
-    print ("Error while connecting to PostgreSQL", error)
+	except (Exception, psycopg2.Error) as error :
+	    print ("Error while connecting to PostgreSQL", error)
 
-finally:
-    if(conn):
-        cur.close()
-        conn.close()
-        print("PostgreSQL connection is closed")    
+	finally:
+	    if(conn):
+	        cur.close()
+	        conn.close()
+	        print("PostgreSQL connection is closed")    
