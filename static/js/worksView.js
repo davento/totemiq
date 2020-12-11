fef061cd59aebc67d9043a08e02d67a67a4105f0
davento <daniela.vento@utec.edu.pe>
2020-12-11 12:46:50 -0500

    works slideshow base done

--- static/js/worksView.js
+++ static/js/worksView.js
@@ -1,16 +1,16 @@
-$(function(){
-    var folder = "static/assets";
-    var divclass = "works_child";
+// $(function(){
+//     var folder = "static/assets";
+//     var divclass = "works_child";
 
-    $.ajax({
-        url : folder,
-        success: function (data) {
-            $(data).find("a").attr("href", function (i, val) {
-                if( val.match(/\.(glb)$/) ) {
-                    $("worksContainer").append( "<div class='"+ divclass +"' id='"+ i +"'><img src='"+ folder + val +"'></div>" );
-                }
-            });
-        }
-    });
-}
-)
+//     $.ajax({
+//         url : folder,
+//         success: function (data) {
+//             $(data).find("a").attr("href", function (i, val) {
+//                 if( val.match(/\.(png)$/) ) {
+//                     $("slideshow_container").append( "<div class='"+ divclass +"' id='"+ i +"'><img src='"+ folder + val +"'></div>" );
+//                 }
+//             });
+//         }
+//     });
+// }
+// )
